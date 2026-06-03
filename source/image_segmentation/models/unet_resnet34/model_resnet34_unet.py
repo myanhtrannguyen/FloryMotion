0e5a5acf41885e3e37d5046f3994190383a91c43
myanhtrannguyen <myanh260305@gmail.com>
2026-06-04 01:29:25 +0700

    fix git bug

--- source/image_segmentation/models/unet_resnet34/model_resnet34_unet.py
+++ source/image_segmentation/models/unet_resnet34/model_resnet34_unet.py
@@ -4,16 +4,11 @@ import torch.nn.functional as F
 from torchvision import models
 
 
-class ConvBlock(nn.Module):
-    """
-    Basic convolution block used in the decoder.
-    Conv -> BN -> ReLU -> Conv -> BN -> ReLU
-    """
-
+class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        self.block = nn.Sequential(
+        self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
@@ -24,136 +19,125 @@ class ConvBlock(nn.Module):
         )
 
     def forward(self, x):
-        return self.block(x)
+        return self.double_conv(x)
 
 
-class DecoderBlock(nn.Module):
-    """
-    Decoder block:
-    Upsample -> Concatenate skip connection -> ConvBlock
-    """
-
-    def __init__(self, in_channels, skip_channels, out_channels):
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, skip_channels, up_channels, out_channels):
         super().__init__()
 
-        self.conv = ConvBlock(
-            in_channels=in_channels + skip_channels,
-            out_channels=out_channels
+        self.up = nn.ConvTranspose2d(
+            in_channels,
+            up_channels,
+            kernel_size=2,
+            stride=2,
         )
 
-    def forward(self, x, skip=None):
-        x = F.interpolate(
-            x,
-            scale_factor=2,
-            mode="bilinear",
-            align_corners=False
+        self.conv = DoubleConv(
+            up_channels + skip_channels,
+            out_channels,
         )
 
-        if skip is not None:
-            if x.shape[-2:] != skip.shape[-2:]:
-                x = F.interpolate(
-                    x,
-                    size=skip.shape[-2:],
-                    mode="bilinear",
-                    align_corners=False
-                )
+    def forward(self, x, skip):
+        x = self.up(x)
 
-            x = torch.cat([x, skip], dim=1)
+        if x.shape[-2:] != skip.shape[-2:]:
+            x = F.interpolate(
+                x,
+                size=skip.shape[-2:],
+                mode="bilinear",
+                align_corners=False,
+            )
 
-        return self.conv(x)
+        x = torch.cat([skip, x], dim=1)
+        x = self.conv(x)
+
+        return x
 
 
 class ResNet34UNet(nn.Module):
-    """
-    U-Net with ResNet34 encoder.
-
-    Encoder:
-        ResNet34 pretrained on ImageNet
-
-    Decoder:
-        U-Net style decoder with skip connections
-
-    Output:
-        Segmentation mask with num_classes channels
-    """
-
     def __init__(self, num_classes=1, pretrained=True):
         super().__init__()
 
-        if pretrained:
-            weights = models.ResNet34_Weights.IMAGENET1K_V1
-        else:
-            weights = None
+        try:
+            weights = models.ResNet34_Weights.DEFAULT if pretrained else None
+            self.encoder = models.resnet34(weights=weights)
+        except AttributeError:
+            self.encoder = models.resnet34(pretrained=pretrained)
 
-        resnet = models.resnet34(weights=weights)
+        # Quan trọng: checkpoint không có encoder.fc.weight/bias
+        self.encoder.fc = nn.Identity()
 
-        # Encoder
-        self.input_block = nn.Sequential(
-            resnet.conv1,   # output: 64 channels, H/2, W/2
-            resnet.bn1,
-            resnet.relu
-        )
-
-        self.maxpool = resnet.maxpool
-
-        self.encoder1 = resnet.layer1  # 64 channels, H/4
-        self.encoder2 = resnet.layer2  # 128 channels, H/8
-        self.encoder3 = resnet.layer3  # 256 channels, H/16
-        self.encoder4 = resnet.layer4  # 512 channels, H/32
-
-        # Decoder
-        self.decoder4 = DecoderBlock(
+        self.up4 = UpBlock(
             in_channels=512,
             skip_channels=256,
-            out_channels=256
+            up_channels=256,
+            out_channels=256,
         )
 
-        self.decoder3 = DecoderBlock(
+        self.up3 = UpBlock(
             in_channels=256,
             skip_channels=128,
-            out_channels=128
+            up_channels=128,
+            out_channels=128,
         )
 
-        self.decoder2 = DecoderBlock(
+        self.up2 = UpBlock(
             in_channels=128,
             skip_channels=64,
-            out_channels=64
+            up_channels=64,
+            out_channels=64,
         )
 
-        self.decoder1 = DecoderBlock(
+        # Quan trọng: checkpoint có up1.up.weight shape [64, 32, 2, 2]
+        # nghĩa là ConvTranspose2d(64 -> 32)
+        # sau đó concat skip 64 channel => 32 + 64 = 96
+        self.up1 = UpBlock(
             in_channels=64,
             skip_channels=64,
-            out_channels=64
+            up_channels=32,
+            out_channels=64,
         )
 
-        self.final_upsample = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False),
-            ConvBlock(64, 32)
+        self.up_final = nn.ConvTranspose2d(
+            64,
+            32,
+            kernel_size=2,
+            stride=2,
         )
 
-        self.segmentation_head = nn.Conv2d(
-            in_channels=32,
-            out_channels=num_classes,
-            kernel_size=1
+        self.final_conv = nn.Sequential(
+            DoubleConv(32, 32),
+            nn.Conv2d(32, num_classes, kernel_size=1),
         )
 
     def forward(self, x):
-        # Encoder
-        x0 = self.input_block(x)      # 64, H/2
-        x1 = self.maxpool(x0)         # 64, H/4
+        x0 = self.encoder.conv1(x)
+        x0 = self.encoder.bn1(x0)
+        x0 = self.encoder.relu(x0)
 
-        e1 = self.encoder1(x1)        # 64, H/4
-        e2 = self.encoder2(e1)        # 128, H/8
-        e3 = self.encoder3(e2)        # 256, H/16
-        e4 = self.encoder4(e3)        # 512, H/32
+        x1 = self.encoder.maxpool(x0)
+        x1 = self.encoder.layer1(x1)
 
-        # Decoder
-        d4 = self.decoder4(e4, e3)    # 256, H/16
-        d3 = self.decoder3(d4, e2)    # 128, H/8
-        d2 = self.decoder2(d3, e1)    # 64, H/4
-        d1 = self.decoder1(d2, x0)    # 64, H/2
+        x2 = self.encoder.layer2(x1)
+        x3 = self.encoder.layer3(x2)
+        x4 = self.encoder.layer4(x3)
 
-        out = self.final_upsample(d1) # 32, H
-        out = self.segmentation_head(out)
+        d4 = self.up4(x4, x3)
+        d3 = self.up3(d4, x2)
+        d2 = self.up2(d3, x1)
+        d1 = self.up1(d2, x0)
+
+        out = self.up_final(d1)
+
+        if out.shape[-2:] != x.shape[-2:]:
+            out = F.interpolate(
+                out,
+                size=x.shape[-2:],
+                mode="bilinear",
+                align_corners=False,
+            )
+
+        out = self.final_conv(out)
 
         return out
